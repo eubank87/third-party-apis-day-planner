8229dd06711059e2db587606f6852fc378ff6525
eubank87 <eubank.ashley@gmail.com>
2020-12-16 15:02:47 -0800

    started pseudo code in js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 // upon page open, date/time are shown at top
+// previously saved tasks generate from local storage (or any empty array if nothing exists).
 // as time moves forward, hour rows change from green (future), current (red) & past (grey).
+// when I click into a text area, I can record my tasks 
+// when I click the save button, tasks are saved to local storage
